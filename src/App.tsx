4b48b102c56dfa7f765cb5960ad0778c52d769df
Aleksey6453 <login6453@gmail.com>
2023-03-07 01:02:07 +0200

    c before fack store api

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,13 @@
-import { create } from 'domain';
-import React, {useState, useContext} from 'react';
-import {createElement as cE} from 'react';
-
-
-
+import React from 'react';
+import Product from './components/Product';
 
 
 function App() {
  
   return (
       <div className='main_test'>
-          <h2>34</h2>  
+          <h2>34</h2> 
+          <Product /> 
       </div>
   )
 
